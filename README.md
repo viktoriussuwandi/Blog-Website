6157ecc4eb89bbf6569c41ba9203f1e7361038fe
Viktorius S <viktorius.tori@gmail.com>
2023-03-17 14:45:04 +0000

    working on routing

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 #### This is a blog website. The project running on : https://replit.com/@ViktoriusSuwand/AppBrewery-python-Day-59-Blog-Capstone-Part-2
 
 ### Technology and Features :
-    * using python Flask
-    * using live external API
-    * using Jinja for data transfer with frontEnd and backEnd
+    * Bootstrap as Front-End CSS Framework
+    * python Flask as Back-End Framework
+    * live external API for blog posts
+    * Jinja for data transfer with Front-End and Back-End
+    * smtp library to get user's contact
+    
+### Project Overview
 
-### Description :
-### Development
-### Dataset Source
+#### Home Page
+![Home Page](static/img/overview-1.jpg)
 
-
+#### Contact Page
+![Contact Page](static/img/overview-2.jpg)
--- main.py
+++ main.py
@@ -3,39 +3,39 @@ from datetime import datetime
 import requests
 
 app = Flask(__name__)
+data = {}
 
 def request_data() :
-  blog_url = 'https://api.npoint.io/0543494d63afb91b8aa0'
-  resp = requests.get(blog_url)
-  resp.raise_for_status()
-  all_posts = resp.json()['posts']
+  blog_url = 'https://api.npoint.io/0543494d63afb91b8aa0'; resp = requests.get(blog_url); resp.raise_for_status(); all_posts = resp.json()['posts']
   return all_posts
-  
+
 @app.route('/samplepost')
 def samplepost() :
-  return render_template('sample_post.html')
+  data = { 'bm_img'  : 'static/img/sample_post-bg.jpg', 'txt_heading' : 'Man must explore, and this is exploration at its greatest',
+    'txt_subheading' : 'Problems look mighty small from 150 miles up', 'posts' : None }
+  return render_template('sample_post.html', data = data)
+
+@app.route('/')
+def home () :
+  data = { 'bm_img' : 'static/img/home-bg.jpg', 'txt_heading' : 'My Web Blog', 'txt_subheading' : 'Wellcome To My Home Blog', 'posts' : request_data() }
+  return render_template('index.html', data = data )
   
 @app.route('/about')
 def about() :
-  return render_template('about.html')
-
-@app.route('/contact')
-def contact() :
-  return render_template('contact.html')  
-  
-@app.route('/')
-def home () :
-  all_posts = request_data()
-  return render_template('index.html', data = all_posts)
+  data = { 'bm_img' : 'static/img/about-bg.jpg', 'txt_heading' : 'About Me', 'txt_subheading' : 'This is what I do.','posts' : None }
+  return render_template('about.html', data = data)
 
 @app.route('/post')
 def post() :
-  all_posts = request_data()
   url_params = request.args
-  _id       = url_params.get('id', None)
-  article   = [ p for p in all_posts if p['id'] == int(_id) ] if _id is not None else all_posts
-  return render_template('post.html', data = article)
+  _id        = url_params.get('id', None)
+  data = { 'bm_img' : 'static/img/home-bg.jpg', 'posts' : [ p for p in request_data() if p['id'] == int(_id) ] if _id is not None else request_data() }
+  return render_template('post.html', data = data)
 
-  
+@app.route('/contact')
+def contact() :
+  data = { 'bm_img' : 'static/img/contact-bg.jpg', 'txt_heading' : 'Contact Me', 'txt_subheading' : 'Have questions? I have answers.', 'posts' : None }
+  return render_template('contact.html', data = data)
+
 if __name__ == "__main__":
   app.run( debug=True, host='0.0.0.0', port = 2000 )
